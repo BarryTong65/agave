eef5580c08f314e0d75a1779288646a273a8c841
BarryTong65 <barrytong.work@gmail.com>
2025-03-04 10:21:47 +0800

    fix: add benchmark

--- programs/sbf/tests/loader.rs
+++ programs/sbf/tests/loader.rs
@@ -90,7 +90,7 @@ macro_rules! with_mock_invoke_context {
 #[test]
 fn test_instruction_count_tuner() {
     println!("Program test_instruction_count_tuner");
-    let program_path = PathBuf::from("program.so");
+    let program_path = PathBuf::from("tests/program.so");
     let mut file = File::open(&program_path).unwrap_or_else(|err| {
         panic!("Failed to open {}: {}", program_path.display(), err);
     });
