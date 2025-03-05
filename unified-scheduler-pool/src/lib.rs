cc75994dba02ae07ed2907b7cb38c120820223c1
BarryTong65 <barrytong.work@gmail.com>
2025-03-05 10:17:16 +0800

    fix: update go mod

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -327,9 +327,11 @@ where
                     // Note that this critical section could block the latency-sensitive replay
                     // code-path via ::take_scheduler().
                     #[allow(unstable_name_collisions)]
-                    idle_inners.extend(scheduler_inners.extract_if(|(_inner, pooled_at)| {
-                        now.duration_since(*pooled_at) > max_pooling_duration
-                    }));
+                    {
+                        idle_inners.extend(scheduler_inners.extract_if(|(_inner, pooled_at)| {
+                            now.duration_since(*pooled_at) > max_pooling_duration
+                        }));
+                    }
                     drop(scheduler_inners);
 
                     let idle_inner_count = idle_inners.len();
@@ -358,11 +360,13 @@ where
                         break;
                     };
                     #[allow(unstable_name_collisions)]
-                    expired_listeners.extend(timeout_listeners.extract_if(
-                        |(_callback, registered_at)| {
-                            now.duration_since(*registered_at) > timeout_duration
-                        },
-                    ));
+                    {
+                        expired_listeners.extend(timeout_listeners.extract_if(
+                            |(_callback, registered_at)| {
+                                now.duration_since(*registered_at) > timeout_duration
+                            },
+                        ));
+                    }
                     drop(timeout_listeners);
 
                     let count = expired_listeners.len();
