cd3f3326e627b71243542768ac313b2f4bbee4b3
BarryTong65 <barrytong.work@gmail.com>
2025-03-07 09:48:04 +0800

    feat: logs

--- programs/bpf_loader/src/lib.rs
+++ programs/bpf_loader/src/lib.rs
@@ -102,10 +102,10 @@ pub fn load_program_from_bytes(
             load_program_metrics,
         )
     }
-    .map_err(|err| {
-        ic_logger_msg!(log_collector, "{}", err);
-        InstructionError::InvalidAccountData
-    })?;
+        .map_err(|err| {
+            ic_logger_msg!(log_collector, "{}", err);
+            InstructionError::InvalidAccountData
+        })?;
     Ok(loaded_program)
 }
 
@@ -137,10 +137,10 @@ pub fn deploy_program(
         programdata,
         Arc::new(deployment_program_runtime_environment),
     )
-    .map_err(|err| {
-        ic_logger_msg!(log_collector, "{}", err);
-        InstructionError::InvalidAccountData
-    })?;
+        .map_err(|err| {
+            ic_logger_msg!(log_collector, "{}", err);
+            InstructionError::InvalidAccountData
+        })?;
     load_elf_time.stop();
     load_program_metrics.load_elf_us = load_elf_time.as_us();
     let mut verify_code_time = Measure::start("verify_code_time");
@@ -371,9 +371,9 @@ fn create_memory_mapping<'a, 'b, C: ContextObject>(
         ),
         MemoryRegion::new_writable(heap, MM_HEAP_START),
     ]
-    .into_iter()
-    .chain(additional_regions)
-    .collect();
+        .into_iter()
+        .chain(additional_regions)
+        .collect();
 
     Ok(if let Some(cow_cb) = cow_cb {
         MemoryMapping::new_with_cow(regions, cow_cb, config, sbpf_version)?
@@ -442,8 +442,8 @@ pub(crate) fn process_instruction_inner(
                 },
             )
         }
-        .map(|_| 0)
-        .map_err(|error| Box::new(error) as Box<dyn std::error::Error>);
+            .map(|_| 0)
+            .map_err(|error| Box::new(error) as Box<dyn std::error::Error>);
     }
 
     // Program Invocation
@@ -505,7 +505,7 @@ pub(crate) fn process_instruction_inner(
             Err(Box::new(instruction_error) as Box<dyn std::error::Error>)
         }
     }
-    .map(|_| 0)
+        .map(|_| 0)
 }
 
 fn process_loader_upgradeable_instruction(
@@ -1376,9 +1376,9 @@ fn process_loader_upgradeable_instruction(
             }
             let (program_len, upgrade_authority_address) =
                 if let Ok(UpgradeableLoaderState::ProgramData {
-                    slot,
-                    upgrade_authority_address,
-                }) = programdata.get_state()
+                              slot,
+                              upgrade_authority_address,
+                          }) = programdata.get_state()
                 {
                     if clock_slot == slot {
                         ic_logger_msg!(log_collector, "Program was deployed in this block already");
@@ -1400,7 +1400,7 @@ fn process_loader_upgradeable_instruction(
             // Verify authority signature
             if !migration_authority::check_id(&provided_authority_address)
                 && provided_authority_address
-                    != upgrade_authority_address.unwrap_or(program_address)
+                != upgrade_authority_address.unwrap_or(program_address)
             {
                 ic_logger_msg!(log_collector, "Incorrect migration authority provided");
                 return Err(InstructionError::IncorrectAuthority);
@@ -1455,7 +1455,7 @@ fn process_loader_upgradeable_instruction(
                         program_len as u32,
                         &program_address,
                     )
-                    .into(),
+                        .into(),
                     &[],
                 )?;
 
@@ -1468,7 +1468,7 @@ fn process_loader_upgradeable_instruction(
                         0,
                         program_len as u32,
                     )
-                    .into(),
+                        .into(),
                     &[],
                 )?;
 
@@ -1477,7 +1477,7 @@ fn process_loader_upgradeable_instruction(
                         &program_address,
                         &provided_authority_address,
                     )
-                    .into(),
+                        .into(),
                     &[],
                 )?;
 
@@ -1488,7 +1488,7 @@ fn process_loader_upgradeable_instruction(
                             &provided_authority_address,
                             &program_address,
                         )
-                        .into(),
+                            .into(),
                         &[],
                     )?;
                 } else if migration_authority::check_id(&provided_authority_address) {
@@ -1498,7 +1498,7 @@ fn process_loader_upgradeable_instruction(
                             &provided_authority_address,
                             &upgrade_authority_address.unwrap(),
                         )
-                        .into(),
+                            .into(),
                         &[],
                     )?;
                 }
@@ -1531,8 +1531,8 @@ fn common_close_account(
     }
     if *authority_address
         != Some(*transaction_context.get_key_of_account_at_index(
-            instruction_context.get_index_of_instruction_account_in_transaction(2)?,
-        )?)
+        instruction_context.get_index_of_instruction_account_in_transaction(2)?,
+    )?)
     {
         ic_logger_msg!(log_collector, "Incorrect authority provided");
         return Err(InstructionError::IncorrectAuthority);
@@ -1558,16 +1558,27 @@ fn execute<'a, 'b: 'a>(
     executable: &'a Executable<InvokeContext<'static>>,
     invoke_context: &'a mut InvokeContext<'b>,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    use std::time::Instant;
+    let total_execute_start = Instant::now();
+    println!("bpf_loader_execute - starting execution");
+
     // We dropped the lifetime tracking in the Executor by setting it to 'static,
     // thus we need to reintroduce the correct lifetime of InvokeContext here again.
+    let transmute_start = Instant::now();
     let executable = unsafe {
         mem::transmute::<&'a Executable<InvokeContext<'static>>, &'a Executable<InvokeContext<'b>>>(
             executable,
         )
     };
+    println!("bpf_loader_execute - transmute took: {:?}", transmute_start.elapsed());
+
+    let context_setup_start = Instant::now();
     let log_collector = invoke_context.get_log_collector();
     let transaction_context = &invoke_context.transaction_context;
     let instruction_context = transaction_context.get_current_instruction_context()?;
+    println!("bpf_loader_execute - context setup took: {:?}", context_setup_start.elapsed());
+
+    let program_id_start = Instant::now();
     let (program_id, is_loader_deprecated) = {
         let program_account =
             instruction_context.try_borrow_last_program_account(transaction_context)?;
@@ -1576,24 +1587,39 @@ fn execute<'a, 'b: 'a>(
             *program_account.get_owner() == bpf_loader_deprecated::id(),
         )
     };
+    println!("bpf_loader_execute - program ID resolution took: {:?}", program_id_start.elapsed());
+
+    let jit_setup_start = Instant::now();
     #[cfg(any(target_os = "windows", not(target_arch = "x86_64")))]
     let use_jit = false;
+    if cfg!(feature = "jit") {
+        println!("jit is enabled");
+    }else{
+        println!("jit is not enabled");
+    }
     #[cfg(all(not(target_os = "windows"), target_arch = "x86_64"))]
     let use_jit = executable.get_compiled_program().is_some();
+    println!("bpf_loader_execute - use_jit initialized to: {}", use_jit);
+
     let direct_mapping = invoke_context
         .get_feature_set()
         .is_active(&bpf_account_data_direct_mapping::id());
+    println!("bpf_loader_execute - JIT/feature setup took: {:?}", jit_setup_start.elapsed());
 
     let mut serialize_time = Measure::start("serialize");
+    println!("bpf_loader_execute - starting parameter serialization");
+    let serialize_start = Instant::now();
     let (parameter_bytes, regions, accounts_metadata) = serialization::serialize_parameters(
         invoke_context.transaction_context,
         instruction_context,
         !direct_mapping,
     )?;
     serialize_time.stop();
+    println!("bpf_loader_execute - parameter serialization took: {:?}", serialize_start.elapsed());
 
     // save the account addresses so in case we hit an AccessViolation error we
     // can map to a more specific error
+    let account_addr_start = Instant::now();
     let account_region_addrs = accounts_metadata
         .iter()
         .map(|m| {
@@ -1608,34 +1634,52 @@ fn execute<'a, 'b: 'a>(
             m.vm_data_addr..vm_end
         })
         .collect::<Vec<_>>();
+    println!("bpf_loader_execute - account region address mapping took: {:?}", account_addr_start.elapsed());
 
     let mut create_vm_time = Measure::start("create_vm");
+    println!("bpf_loader_execute - starting VM creation");
+    let vm_start = Instant::now();
     let execution_result = {
         let compute_meter_prev = invoke_context.get_remaining();
+        println!("bpf_loader_execute - before VM creation, use_jit value: {}", use_jit);
         create_vm!(vm, executable, regions, accounts_metadata, invoke_context);
+        let create_vm_took = vm_start.elapsed();
+        println!("bpf_loader_execute - VM creation took: {:?}", create_vm_took);
+
+        let vm_setup_start = Instant::now();
         let (mut vm, stack, heap) = match vm {
             Ok(info) => info,
             Err(e) => {
                 ic_logger_msg!(log_collector, "Failed to create SBF VM: {}", e);
+                println!("bpf_loader_execute - VM creation failed: {}", e);
                 return Err(Box::new(InstructionError::ProgramEnvironmentSetupFailure));
             }
         };
         create_vm_time.stop();
+        println!("bpf_loader_execute - VM setup took: {:?}", vm_setup_start.elapsed());
 
+        let execution_start = Instant::now();
         vm.context_object_pointer.execute_time = Some(Measure::start("execute"));
+        println!("bpf_loader_execute - executing program, use_jit value: {}", use_jit);
         let (compute_units_consumed, result) = vm.execute_program(executable, !use_jit);
-        MEMORY_POOL.with_borrow_mut(|memory_pool| {
-            memory_pool.put_stack(stack);
-            memory_pool.put_heap(heap);
-            debug_assert!(memory_pool.stack_len() <= MAX_INSTRUCTION_STACK_DEPTH);
-            debug_assert!(memory_pool.heap_len() <= MAX_INSTRUCTION_STACK_DEPTH);
-        });
+        let execution_took = execution_start.elapsed();
+        println!("bpf_loader_execute - program execution took: {:?}", execution_took);
+
+        let cleanup_start = Instant::now();
+        // MEMORY_POOL.with_borrow_mut(|memory_pool| {
+        //     memory_pool.put_stack(stack);
+        //     memory_pool.put_heap(heap);
+        //     debug_assert!(memory_pool.stack_len() <= MAX_INSTRUCTION_STACK_DEPTH);
+        //     debug_assert!(memory_pool.heap_len() <= MAX_INSTRUCTION_STACK_DEPTH);
+        // });
         drop(vm);
         if let Some(execute_time) = invoke_context.execute_time.as_mut() {
             execute_time.stop();
             invoke_context.timings.execute_us += execute_time.as_us();
         }
+        println!("bpf_loader_execute - VM cleanup took: {:?}", cleanup_start.elapsed());
 
+        let logging_start = Instant::now();
         ic_logger_msg!(
             log_collector,
             "Program {} consumed {} of {} compute units",
@@ -1648,12 +1692,17 @@ fn execute<'a, 'b: 'a>(
         if !return_data.is_empty() {
             stable_log::program_return(&log_collector, &program_id, return_data);
         }
-        match result {
+        println!("bpf_loader_execute - logging took: {:?}", logging_start.elapsed());
+
+        let result_processing_start = Instant::now();
+        let result = match result {
             ProgramResult::Ok(status) if status != SUCCESS => {
+                println!("bpf_loader_execute - program returned error status: {:?}", status);
                 let error: InstructionError = status.into();
                 Err(Box::new(error) as Box<dyn std::error::Error>)
             }
             ProgramResult::Err(mut error) => {
+                println!("bpf_loader_execute - program execution failed: {:?}", error);
                 if invoke_context
                     .get_feature_set()
                     .is_active(&solana_feature_set::deplete_cu_meter_on_vm_failure::id())
@@ -1720,8 +1769,13 @@ fn execute<'a, 'b: 'a>(
                     error.into()
                 })
             }
-            _ => Ok(()),
-        }
+            _ => {
+                println!("bpf_loader_execute - program executed successfully");
+                Ok(())
+            }
+        };
+        println!("bpf_loader_execute - result processing took: {:?}", result_processing_start.elapsed());
+        result
     };
 
     fn deserialize_parameters(
@@ -1741,17 +1795,23 @@ fn execute<'a, 'b: 'a>(
     }
 
     let mut deserialize_time = Measure::start("deserialize");
+    println!("bpf_loader_execute - starting parameter deserialization");
+    let deserialize_start = Instant::now();
     let execute_or_deserialize_result = execution_result.and_then(|_| {
         deserialize_parameters(invoke_context, parameter_bytes.as_slice(), !direct_mapping)
             .map_err(|error| Box::new(error) as Box<dyn std::error::Error>)
     });
     deserialize_time.stop();
+    println!("bpf_loader_execute - parameter deserialization took: {:?}", deserialize_start.elapsed());
 
     // Update the timings
+    let timing_update_start = Instant::now();
     invoke_context.timings.serialize_us += serialize_time.as_us();
     invoke_context.timings.create_vm_us += create_vm_time.as_us();
     invoke_context.timings.deserialize_us += deserialize_time.as_us();
+    println!("bpf_loader_execute - timing update took: {:?}", timing_update_start.elapsed());
 
+    println!("bpf_loader_execute - total execution time: {:?}", total_execute_start.elapsed());
     execute_or_deserialize_result
 }
 
@@ -2163,7 +2223,7 @@ mod tests {
             offset: 0,
             bytes: vec![42; 9],
         })
-        .unwrap();
+            .unwrap();
         process_instruction(
             &loader_id,
             &[],
@@ -2178,7 +2238,7 @@ mod tests {
             offset: 0,
             bytes: vec![42; 9],
         })
-        .unwrap();
+            .unwrap();
         buffer_account
             .set_state(&UpgradeableLoaderState::Buffer {
                 authority_address: Some(buffer_address),
@@ -2214,7 +2274,7 @@ mod tests {
             offset: 3,
             bytes: vec![42; 6],
         })
-        .unwrap();
+            .unwrap();
         let mut buffer_account =
             AccountSharedData::new(1, UpgradeableLoaderState::size_of_buffer(9), &loader_id);
         buffer_account
@@ -2252,7 +2312,7 @@ mod tests {
             offset: 0,
             bytes: vec![42; 10],
         })
-        .unwrap();
+            .unwrap();
         buffer_account
             .set_state(&UpgradeableLoaderState::Buffer {
                 authority_address: Some(buffer_address),
@@ -2272,7 +2332,7 @@ mod tests {
             offset: 1,
             bytes: vec![42; 9],
         })
-        .unwrap();
+            .unwrap();
         buffer_account
             .set_state(&UpgradeableLoaderState::Buffer {
                 authority_address: Some(buffer_address),
@@ -2292,7 +2352,7 @@ mod tests {
             offset: 0,
             bytes: vec![42; 9],
         })
-        .unwrap();
+            .unwrap();
         buffer_account
             .set_state(&UpgradeableLoaderState::Buffer {
                 authority_address: Some(buffer_address),
@@ -2324,7 +2384,7 @@ mod tests {
             offset: 1,
             bytes: vec![42; 9],
         })
-        .unwrap();
+            .unwrap();
         buffer_account
             .set_state(&UpgradeableLoaderState::Buffer {
                 authority_address: Some(buffer_address),
@@ -2358,7 +2418,7 @@ mod tests {
             offset: 1,
             bytes: vec![42; 9],
         })
-        .unwrap();
+            .unwrap();
         buffer_account
             .set_state(&UpgradeableLoaderState::Buffer {
                 authority_address: None,
@@ -3888,7 +3948,7 @@ mod tests {
             &bincode::serialize(&UpgradeableLoaderInstruction::DeployWithMaxDataLen {
                 max_data_len: 0,
             })
-            .unwrap(),
+                .unwrap(),
             vec![
                 (recipient_address, recipient_account),
                 (programdata_address, programdata_account),
